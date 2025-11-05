7942d3dbc6ed04dd271cbbdc05d93f2b181f0823
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-11-05 02:03:01 -0500

    updated EventDatase both interface and class

--- EventDatabase.java
+++ EventDatabase.java
@@ -1,3 +1,56 @@
-public class EventDatabse implements EventDatabaseInterface{
-    private ArrayList<Event> eventList = new ArrayList<Event>;
+import java.util.ArrayList;
+
+public class EventDatabase implements EventDatabaseInterface{
+    private ArrayList<Event> eventList = new ArrayList<Event>();
+
+    //pass nothing
+    public EventDatabase() {
+        eventList = new ArrayList<Event>();
+
+    }
+    //pass a single event
+    public EventDatabase(Event singleEvent) {
+        eventList.add(singleEvent);
+    }
+    //pass list of events
+    public EventDatabase(ArrayList<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+
+    //getters setters
+    public ArrayList<Event> getEvents() {
+        return eventList;
+    }
+
+    public void setEvents(ArrayList<Event> listInput) {
+        this.eventList = listInput;
+    }
+
+
+    /**
+     * Public method to add a single event to the list of events
+     * @param eventInput
+     */
+    public void addEvent(Event eventInput) {
+        this.eventList.add(eventInput);
+    }
+
+    /**
+     * given an int, the event at that index in eventList will be removed.
+     * @param eventNum
+     */
+    public void rmEvent(int eventNum){
+        try {
+            this.eventList.remove(eventNum);
+        }
+        catch(IndexOutOfBoundsException e) {
+            System.out.println("Error: event number intputted is not a valid index");
+            e.printStackTrace();
+            return;
+        }
+
+    }
+
 }
+
--- EventDatabaseInterface.java
+++ EventDatabaseInterface.java
@@ -7,24 +7,8 @@ public interface EventDatabaseInterface {
     public ArrayList<Event> getEvents();
 
     //public method to set the list of events
-    public void setEvents(ArrayList<Event>);
-
-    //public method to add an event to the list of events
-    public default void addEvent(Event) {
-        this.eventList.add(Event);
-    }
+    public void setEvents(ArrayList<Event> listInput);
 
 
-    public void default rmEvent(int eventNum){
-        try {
-            this.eventList.remove(eventNum);
-        }
-        catch(IndexOutOfBoundsException e) {
-            System.out.println("Error: event number intputted is not a valid index");
-            e.printStackTrace();
-            return;
-        }
-
-    }
 
 }
