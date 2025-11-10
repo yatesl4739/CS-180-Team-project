37efef381af45ebbe6491a3ab4c4ac752f02f3b9
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-11-10 18:41:42 -0500

    Updated EventTest

--- EventTest.java
+++ EventTest.java
@@ -1,31 +1,73 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class EventTest {
+    @Test
     public void eventConstructor() {
-        char[][] chart = {{'x','y'}};
-        Event e = new Event("Movie", 10.0, chart, 1000L, 20L);
-        assertEquals("Movie", e.getEventName());
-        assertEquals(10.0, e.getPriceOfEvent(), 0.001);
-        assertEquals(1000L, e.getTimeOfDay());
-        assertEquals(10L, e.getDay());
-        assertArrayEquals(chart, e.getSeatingChart());
+        char[][] seatingChart = {
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', ' ', ' ', ' ', 'o'}};
+        long date = ChronoUnit.DAYS.between(
+                LocalDate.ofEpochDay(0), LocalDate.now()
+        );
+        Event e = new Event("DJ Khalid", 10.0, seatingChart, 360, date);
+        assertEquals("Testing event name","DJ Khalid", e.getEventName());
+        assertEquals("Testing price of event",10.0, e.getPriceOfEvent());
+        assertEquals("Testing time of day", 360, e.getTimeOfDay());
+        assertEquals("Testing date",date, e.getDay());
+        assertArrayEquals("Testing seating chart", seatingChart, e.getSeatingChart());
     }
 
+    @Test
     public void eventSetters() {
-        char[][] chart = {{'x','y'}};
-        Event e = new Event("Movie", 10.0, chart, 1000L, 20L);
-        e.setEventName("Show");
+        char[][] seatingChart = {
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'x', 'o'},
+                {'o', ' ', ' ', ' ', 'o'}};
+        long date = ChronoUnit.DAYS.between(
+                LocalDate.ofEpochDay(0), LocalDate.now()
+        );
+        Event e = new Event("Drake", 10.0, seatingChart, 360, date);
+        e.setEventName("Benson Boone");
         e.setPriceOfEvent(15.0);
-        e.setTimeOfDay(2000L);
-        e.setDay(50L);
-        char[][] newChart = {{'x'}};
+        e.setTimeOfDay(420);
+        e.setDay(date + 1);
+        char[][] newChart = {
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'o', 'o'},
+                {'o', 'o', 'o', 'o', 'o'}};
         e.setSeatingChart(newChart);
 
-        assertEquals("Show", e.getEventName());
-        assertEquals(15.0, e.getPriceOfEvent(), 0.001);
-        assertEquals(2000L, e.getTimeOfDay());
-        assertEquals(50L, e.getDay());
+        assertEquals("Benson Boone", e.getEventName());
+        assertEquals(15.0, e.getPriceOfEvent());
+        assertEquals(429, e.getTimeOfDay());
+        assertEquals(date + 1, e.getDay());
         assertArrayEquals(newChart, e.getSeatingChart());
     }
+
+    @Test
+    public void testToString() {
+        User u = new User("bob", "pw");
+        long date = ChronoUnit.DAYS.between(
+                LocalDate.ofEpochDay(0), LocalDate.now()
+        );
+        Event e = new Event("Concert", 20.0,
+                new char[][]{
+                           {'o', 'o', 'o', 'o'},
+                        {'o', 'o', 'o', 'o', 'o'},
+                        {'o', 'o', 'o', 'o', 'o'},
+                           {'o', 'o', 'o', 'o'}},
+                405, date);
+
+        e.createReservation(new int[]{0,0}, new int[]{1,2}, u, 2, 405, date);
+
+
+    }
 }
+
