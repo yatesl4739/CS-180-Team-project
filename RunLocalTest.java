df98e81d148163b4c09aba20afb5657c567acd44
lbzhang20 <lbzhang20@gmail.com>
2025-11-10 11:12:48 -0500

    junit

--- RunLocalTest.java
+++ RunLocalTest.java
@@ -91,5 +91,34 @@ public class RunLocalTest {
         assertEquals("Setter failed", 50.0, r.getPrice(), 0.001);
         assertArrayEquals("Setter failed", newSeats, r.getReservedSeats());
     }
+
+
+    public void eventConstructor() {
+        char[][] chart = {{'x','y'}};
+        Event e = new Event("Movie", 10.0, chart, 1000L, 20L);
+
+        assertEquals("Movie", e.getEventNAme());
+        assertEquals(10.0, e.getPriceOfEvent(), 0.001);
+        assertEquals(1000L, e.getTimeOfDay());
+        assertEquals(10L, e.getDay());
+        assertArrayEquals(chart, e.getSeatingChart());
+    }
+
+    public void eventSetters() {
+        char[][] chart = {{'x','y'}};
+        Event e = new Event("Movie", 10.0, chart, 1000L, 20L);
+        e.setEventName("Show");
+        e.setPriceOfEvent(15.0);
+        e.setTimeOfDay(2000L);
+        e.setDay(50L);
+        char[][] newChart = {{'x'}};
+        e.setSeatingChart(newChart);
+
+        assertEquals("Show", e.getEventName());
+        assertEquals(15.0, e.getPriceOfEvent(), 0.001);
+        assertEquals(2000L, e.getTimeOfDay());
+        assertEquals(50L, e.getDay());
+        assertArrayEquals(newChart, e.getSeatingChart());
+    }
 }
 
