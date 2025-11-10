fcf41fa33b9e1bf42839fae9c2469dd9e9f6b0da
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-11-10 02:45:59 -0500

    simple fixes to UserDatabase

--- UserDatabase.java
+++ UserDatabase.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class UserDatabase implements UserDatbaseInterface {
+public class UserDatabase implements UserDatabaseInterface {
 
     private ArrayList<User> userDb = new ArrayList<User>();
 
@@ -38,7 +38,14 @@ public class UserDatabase implements UserDatbaseInterface {
 
     // Remove User by index, return true if succesful
     public boolean removeUser(int index) {
-        return userDb.remove(index);
+
+        try {
+            userDb.remove(index);
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index provided was out of bounds");
+            return false;
+        }
     }
 
 }
