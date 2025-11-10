f0c35d6b5d61eae2dfaf1e9e1edb373908665374
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-11-09 19:09:19 -0500

    wrote a method to create a new reservation for a certain event. Needs an update to the reservation object and reservationDatabse

--- Event.java
+++ Event.java
@@ -3,14 +3,42 @@ public class Event {
     private char[][] seatingChart; //stores available seats and shape of seats
     private long timeOfDay; // time since epoch
     private long day;
+    private ReservationDatabase reservatinDB = new ReservationDatabase();
+    private long totalRevenue;
 
+    /**
+     * constructor of object without taking totalRevenue as a modified input
+     * totalRevenue is by default set to 0
+     * @param priceOfEvent
+     * @param seatingChart
+     * @param timeOfDay
+     * @param day
+     */
     public Event(double priceOfEvent, char[][] seatingChart, long timeOfDay, long day){
         this.priceOfEvent = priceOfEvent;
         this.seatingChart = seatingChart;
         this.timeOfDay = timeOfDay;
         this.day = day;
+        this.totalRevenue = 0;
     }
-    private ReservationDatabase reservationDb = new ReservationDatabase();
+
+    /**
+     * constructor of object where totalRevenue is not set to default
+     *
+     * @param priceOfEvent
+     * @param seatingChart
+     * @param timeOfDay
+     * @param day
+     * @param totalRevenue
+     */
+    public Event(double priceOfEvent, char[][] seatingChart, long timeOfDay, long day, long totalRevenue){
+        this.priceOfEvent = priceOfEvent;
+        this.seatingChart = seatingChart;
+        this.timeOfDay = timeOfDay;
+        this.day = day;
+        this.totalRevenue = totalRevenue;
+    }
+
 
     //getters setters
 
@@ -44,6 +72,52 @@ public class Event {
 
     public long getDay() {
         return day;
+    }
+
+    /**
+     * uses input from UI to create a reservation for this event.
+     * @param x
+     * @param y
+     * @param user
+     * @param numPeople
+     * @param timeOfReservation
+     * @param date
+     * @return
+     */
+    public boolean createReservation(int[] x, int[] y, String user, int numPeople, long timeOfReservation, long date) {
+        //array of x values represents x values of seats reserved
+        //array of y values represents y values of sears reserved
+
+        double totalCostOfReservation = numPeople * priceOfEvent;
+
+        if (x.length != y.length) {
+            return false;
+            //Error beacuse there is a mismatch in data provided for seat position
+        }
+        for (int i = 0; i < x.length; i++) {
+            if (seatingChart[x[i]][y[i]] == ' ') {
+                System.out.println("the seat assignemnt at index " + i
+                        + "does not exist in the seating chart");
+                return false;
+                //seat does not exist
+            } else if (seatingChart[x[i]][y[i]] == 'x') {
+                System.out.println("the seat assignemnt at index " + i
+                        + "is already reserved");
+                return false;
+                //seat is taken
+            } else if (seatingChart[x[i]][y[i]] == 'o') {
+                System.out.println("Seat # " + (i + 1) + " has been resved");
+                //seat is reserved and now marked as taken
+            }
+
+        }
+        reservationDB.addReservation(new Reservation(this, user, numPeople, timeOfReservation, date,
+                totalCostOfReservation, x, y));
+    }
+
+
+
+
     }
 
 
