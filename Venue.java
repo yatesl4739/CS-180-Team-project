b4734e6185a43be9755c08718a2454a834fef789
krish talati <56898296+yoinc-creator@users.noreply.github.com>
2025-11-09 23:47:17 -0500

    Update Venue.java

--- Venue.java
+++ Venue.java
@@ -1,6 +1,16 @@
-import java.util.ArrayList;
+import java.util.*;
+
+
+/**
+ * The Venue class represents a physical location that hosts multiple events.
+ * Each Venue contains its own EventDatabase, which stores all events occurring
+ * at that venue. The Venue also stores information about its name and hours
+ * of operation.
+ */
 
 public class Venue implements VenueInterface{
+
+    private final Object gatekeeper = new Object();
 
     //the name of the venue (e.g "AMC theaters" or "Reagal cinemas")
     private String venueName;
@@ -9,18 +19,33 @@ public class Venue implements VenueInterface{
     // Stores all events for this venue using the EventDatabase class
     private EventDatabase eventDatabase;
 
-
+    /**
+     * Constructor for Venue that initializes only the name.
+     * Hours of operation default to "Not specified" and a new EventDatabase is created.
+     *
+     * @param name The name of the venue.
+     */
     public Venue(String name) {
         this.venueName = name;
         this.hoursOfOperation = "Not specified";
         this.eventDatabase = new EventDatabase();
     }
 
+    /**
+     * Constructor for Venue that initializes name, database, and hours of operation.
+     *
+     * @param name             The name of the venue.
+     * @param eventDatabase    The EventDatabase associated with the venue.
+     * @param hoursOfOperation The operating hours of the venue.
+     */
     public Venue(String name, EventDatabase eventDatabase, String hoursOfOperation) {
         this.venueName = name;
         this.eventDatabase = eventDatabase;
         this.hoursOfOperation = hoursOfOperation;
     }
+
+
+    //getters and setters
 
     public String getHoursOfOperation(){
         return this.hoursOfOperation;
@@ -46,29 +71,58 @@ public class Venue implements VenueInterface{
         this.eventDatabase = eventDatabase;
     }
 
+
+    /**
+     * Returns a list of all event times for events in the database.
+     *
+     * @return ArrayList of event times (in epoch format).
+     */
     public ArrayList<Long> getEventTimes(){
-        ArrayList<Long> times = new ArrayList<>();
-        for(Event e: eventDatabase.getEvents()){
-            times.add(e.getTimeOfDay());
+
+        synchronized (gatekeeper){
+            ArrayList<Long> times = new ArrayList<>();
+            for(Event e: eventDatabase.getEvents()){
+                times.add(e.getTimeOfDay());
+            }
+            return times;
         }
-        return times;
+
     }
 
+    /**
+     * Returns a list of all event prices for events in the database.
+     *
+     * @return ArrayList of event prices.
+     */
     public ArrayList<Double> getEventPrices(){
-        ArrayList<Double> prices = new ArrayList<>();
-        for(Event e: eventDatabase.getEvents()){
-            prices.add(e.getPriceOfEvent());
+        synchronized (gatekeeper){
+            ArrayList<Double> prices = new ArrayList<>();
+            for(Event e: eventDatabase.getEvents()){
+                prices.add(e.getPriceOfEvent());
+            }
+            return prices;
         }
-        return prices;
+
     }
 
+    /**
+     * Adds a new event to the venue’s EventDatabase.
+     *
+     * @param e The Event to add.
+     */
     public void addEvent(Event e){
         eventDatabase.addEvent(e);
     }
 
+    /**
+     * Removes an event from the venue’s EventDatabase based on its index.
+     *
+     * @param index The index of the event to remove.
+     */
     public void removeEvent(int index){
         eventDatabase.rmEvent(index);
     }
+
 
 
 }
